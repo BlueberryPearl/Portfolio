574cc8d6448ae3808793eba186ba9e1c5d2e91a5
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-13 18:00:40 +0100

    Styling the Projects component WIP

--- src/About/About.js
+++ src/About/About.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './About.css'
-import Picture from './Blueberry2.png'
+
 
 export default function About() {
     return (
--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -1,23 +1,26 @@
 import React from "react";
-import './Projects.css'
+import './Projects.css';
+import dbc from './dbcThumnail.png'
+import themeThumbnail from './themeThumbnail.png'
+import tj from './tjThumbnail..png'
 
 export default function Projects() {
     return (
         <main className="my-projects">
-            <h1 className="my-projects-title">Projects</h1>
+            <h1 className="my-projects-title">My Projects</h1>
             <div className="projects">
                 <div className="dbc">
-                    <img></img>
+                    <img className="project-images" src={dbc} alt="Digital business card"></img>
                     <h2 className="project-title">Digital Business Card</h2>
                     <p className="project-description">This is my first React project! I created a digital business card to give a brief introduction about myself. It contains some interactive buttons and icons as well as still text and an image. </p>
                 </div>
                 <div className="travel-journal">
-                    <img></img>
+                    <img className="project-images" src={tj} alt="Travel Journal"></img>
                     <h2 className="project-title">Travel Journal</h2>
                     <p className="project-description">This is my second React project. I focused on making a clear yet appealing design. I also added links to all the locations mentioned</p>
                 </div>
                 <div className="cyclone-station">
-                    <img></img>
+                    <img className="project-images" src={themeThumbnail} alt="Cyclone Station"></img>
                     <h2 className="project-title">Cyclone Station</h2>
                     <p className="project-description">This is my first personal project in React. It is a theme generator to help get some inspiration for making art. I am constantly adding new features to it. My next action will be to add the Login and Sign up features to it.
                     </p>
