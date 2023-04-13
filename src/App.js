0c34f52c5425a56d96997f0a556ae5537a78d20c
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-13 14:33:49 +0100

    Created the Projects component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import Navbar from "./Navbar/Navbar";
 import About from "./About/About";
+import Projects from "./Projects/Projects";
 
 function App() {
   return (
     <div className="App">
       <Navbar />
       <About />
+      <Projects />
     </div>
   );
 }
