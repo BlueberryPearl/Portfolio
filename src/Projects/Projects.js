0ce61628501256108da9c2cac6233ad098805a79
Blueberry Pearl <120589100+BlueberryPearl@users.noreply.github.com>
2023-04-25 10:41:27 +0100

    Added links to dbc and travel journal

--- src/Projects/Projects.js
+++ src/Projects/Projects.js
@@ -10,17 +10,17 @@ export default function Projects(props) {
             <h1 className="my-projects-title">My Projects</h1>
             <div className="projects">
                 <div className="dbc">
-                    <img className="project-images" src={dbc} alt="Digital business card"></img>
+                    <a href="https://blueberrypearl.github.io/Digital-Business-Card/" target='_blank'><img className="project-images" src={dbc} alt="Digital business card"></img></a>
                     <h2 className="project-title">Digital Business Card</h2>
                     <p className="project-description">This is my first React project! I created a digital business card to give a brief introduction about myself. It contains some interactive buttons and icons as well as still text and an image. </p>
                 </div>
                 <div className="travel-journal">
-                    <img className="project-images" src={tj} alt="Travel Journal"></img>
+                    <a href="https://blueberrypearl.github.io/Travel-Journal/" target='_blank'><img className="project-images" src={tj} alt="Travel Journal"></img></a>
                     <h2 className="project-title">Travel Journal</h2>
                     <p className="project-description">This is my second React project. I focused on making a clear yet appealing design. I also added links to all the locations mentioned</p>
                 </div>
                 <div className="cyclone-station">
-                    <img className="project-images" src={themeThumbnail} alt="Cyclone Station"></img>
+                    <a href="" target='_blank'><img className="project-images" src={themeThumbnail} alt="Cyclone Station"></img></a>
                     <h2 className="project-title">Cyclone Station</h2>
                     <p className="project-description">This is my first personal project in React. It is a theme generator to help get some inspiration for making art. I am constantly adding new features to it. My next action will be to add the Login and Sign up features to it.
                     </p>
